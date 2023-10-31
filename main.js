ec6cc58ef3831099e4f81e1eb4ce753cb17d0f2a
root <root@foundry-vtt.northamerica-northeast2-b.c.my-project-1470663287257.internal>
2023-10-31 03:23:43 +0000

    init

---
+++ main.js
@@ -0,0 +1,70 @@
+import { registerSettings } from "./modules/settings.js";
+
+export let i18n = key => {
+    return game.i18n.localize(key);
+};
+
+export let setting = key => {
+    return game.settings.get("world-notification", key);
+};
+
+export let patchFunc = (prop, func, type = "WRAPPER") => {
+    let nonLibWrapper = () => {
+        const oldFunc = eval(prop);
+        eval(`${prop} = function (event) {
+            return func.call(this, ${type != "OVERRIDE" ? "oldFunc.bind(this)," : ""} ...arguments);
+        }`);
+    }
+    if (game.modules.get("lib-wrapper")?.active) {
+        try {
+            libWrapper.register("world-notification", prop, func, type);
+        } catch (e) {
+            nonLibWrapper();
+        }
+    } else {
+        nonLibWrapper();
+    }
+}
+
+export let sendMessage = (webhookURL, payload) => {
+      var request = new XMLHttpRequest();
+
+      request.open("POST", webhookURL);
+      request.setRequestHeader('Content-type', 'application/json');
+      request.send(JSON.stringify(payload));
+}
+
+export class WorldNotification extends Application {
+}
+
+Hooks.on("init", () => {
+    registerSettings();
+});
+
+Hooks.on("ready", () => {
+    if (game.user.isGM) {
+	let worldnamePlaceholder = "%worldname%";
+        let webhook = setting("webhook");
+	let username = setting("username");
+	let avatar = setting("avatar");
+	let openMessage = setting("open-message").replace(worldnamePlaceholder, game.world.title);
+	let closeMessage = setting("close-message").replace(worldnamePlaceholder, game.world.title);
+        let payload = {
+            username: username,
+	    avatar_url: avatar,
+            content: openMessage
+        };
+
+        sendMessage(webhook, payload);
+        patchFunc("Game.prototype.shutDown", async function (wrapped, ...args) {
+            try {
+		payload.content = closeMessage;
+                sendMessage(webhook, payload);
+            } catch(e) {
+                console.log("Wrapping Error!");
+            }
+            return wrapped(...args);
+        });
+    }
+});
+
---
+++ modules/settings.js
@@ -0,0 +1,58 @@
+import { i18n, setting } from "../main.js";
+
+export const debouncedReload = foundry.utils.debounce(() => window.location.reload(), 100);
+
+export const registerSettings = function () {
+    let modulename = "world-notification";
+
+    game.settings.register(modulename, "webhook", {
+        name: i18n("WORLDNOTIFICATION.webhook.name"),
+        hint: i18n("WORLDNOTIFICATION.webhook.hint"),
+        scope: "world",
+        default: "",
+        type: String,
+        onChange: debouncedReload,
+        config: true
+    });
+
+    game.settings.register(modulename, "username", {
+        name: i18n("WORLDNOTIFICATION.username.name"),
+        hint: i18n("WORLDNOTIFICATION.username.hint"),
+        scope: "world",
+        default: "Heimdall",
+        type: String,
+        onChange: debouncedReload,
+        config: true
+    });
+
+    game.settings.register(modulename, "avatar", {
+        name: i18n("WORLDNOTIFICATION.avatar.name"),
+        hint: i18n("WORLDNOTIFICATION.avatar.hint"),
+        scope: "world",
+        default: "",
+        type: String,
+        onChange: debouncedReload,
+        config: true
+    });
+
+    game.settings.register(modulename, "open-message", {
+        name: i18n("WORLDNOTIFICATION.open-message.name"),
+        hint: i18n("WORLDNOTIFICATION.open-message.hint"),
+        scope: "world",
+        default: "Heros, the portal to %worldname% has opened. Be warned, I shall uphold my sacred oath to protect this realm as its gatekeeper. If you return threatens the safty of our homeland, my gate will remain shut and you will be left to die.",
+        type: String,
+        onChange: debouncedReload,
+        config: true
+    });
+
+    game.settings.register(modulename, "close-message", {
+        name: i18n("WORLDNOTIFICATION.close-message.name"),
+        hint: i18n("WORLDNOTIFICATION.close-message.hint"),
+        scope: "world",
+        default: "Hurry up! The %worldname% portal is closing, I couldn't hold it anymore. Allfathers, let the dark magic flow through me one last ... time.",
+        type: String,
+        onChange: debouncedReload,
+        config: true
+    });
+
+}
